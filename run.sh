ab48a4ce850041b0f2140a544dc3c96f23097d71
Patrick Steiner <patrick@steiner-buchholz.de>
2016-03-03 13:41:58 +0100

    first running version

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export deviceType="temperature"
+export deviceID="4711"
+export initialValue="20"
+export count="50"
+export waitTime="1"
+export receiverURL="localhost"
+export brokerUID="admin"
+export brokerPassword="change12_me"
+
+echo "Starting the producer to send messages "
+java -DbrokerUID=$brokerUID -DbrokerPassword=$brokerPassword -DreceiverURL=$receiverURL -DdeviceType=$deviceType -DdeviceID=$deviceID -DinitialValue=$initialValue -Dcount=$count -DwaitTime=$waitTime -jar Software_Sensor/target/softwareSensor-jar-with-dependencies.jar
